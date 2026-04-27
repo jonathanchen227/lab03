2e46fe0db9cf67818aa0fff43e26cab711772c4d
Jonathan Chen <jonathanchen227@csilvm-20.cs.ucsb.edu>
2026-04-27 00:22:14 -0700

    main

---
+++ IceCreamItem.h
@@ -0,0 +1,24 @@
+#ifndef ICECREAMITEM_H
+#define ICECREAMITEM_H
+
+#include <string>
+
+class IceCreamItem {
+public:
+	// Constructor for an IceCreamItem. All ice cream items
+	// will either be "small", "medium", or "large".
+	IceCreamItem(std::string s);
+
+	// A virtual method that composes a string with the details
+	// of an IceCreamItem. See the lab writeup and test files
+	// for examples of the format for composeItem.
+	virtual std::string composeItem();
+        virtual ~IceCreamItem() = default;
+	// Returns the dollar amount of an ice cream item.
+	virtual double getPrice();
+protected:
+	double price;
+	std::string size;
+};
+
+#endif
